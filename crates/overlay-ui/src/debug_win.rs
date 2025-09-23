1414b0394eee9433975fa3774c5e53a44ca6540c
laVashik <matiyukv41@gmail.com>
2025-09-23 14:18:17 +0300

    feat: Implement example of using on_raw_input
    The example toggles force_draw with the Q key.

--- crates/overlay-ui/src/debug_win.rs
+++ crates/overlay-ui/src/debug_win.rs
@@ -1,17 +1,30 @@
 use crate::{SharedState, Window};
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct DebugWindow {
-    pub is_open: bool,
+    pub is_closed: bool,
+    force_draw: bool,
 }
 
 impl Window for DebugWindow {
     fn name(&self) -> &'static str { "Debug Window" }
-    fn toggle(&mut self) { self.is_open = !self.is_open; }
-    fn is_open(&self) -> bool { self.is_open }
+    fn toggle(&mut self) { self.is_closed = !self.is_closed; }
+    fn is_open(&self) -> bool { !self.is_closed }
+
+    fn on_raw_input(&mut self, umsg: u32, wparam: u16) -> bool {
+        // Handle keyup messages.
+        if umsg == windows::Win32::UI::WindowsAndMessaging::WM_KEYUP {
+            // Toggle force_draw on Q keyup.
+            if wparam == windows::Win32::UI::Input::KeyboardAndMouse::VK_Q.0 {
+                self.force_draw = !self.force_draw;
+            }
+        }
+
+        true // input should be passed to the game
+    }
 
     fn draw(&mut self, ctx: &egui::Context, shared_state: &mut SharedState, engine: &engine_api::Engine) {
-        if !shared_state.is_overlay_focused {
+        if !shared_state.is_overlay_focused && !self.force_draw {
             return
         }
 
